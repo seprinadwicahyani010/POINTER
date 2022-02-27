7b30cfec937f261307377db94a575f3e2a8972a0
seprinadwicahyani010 <99011087+seprinadwicahyani010@users.noreply.github.com>
2022-02-27 20:39:36 +0700

    Add files via upload
    Nama : Seprina Dwi Cahyani
    NPM : G1A021010
    Kelas : Informatika B
    Struktur Data dan Algoritma (SDA)
    Materi Pointer

---
+++ G1A021010 Case 1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a = 10;
+	int b = 15;
+	int c = 27;
+	int *p1;
+	int *p2;
+	int *p3;
+
+	p1 = &b;
+	p2 = p1;
+	p1 = &c;
+	a = *p1;
+	p3 = &b;
+	*p2 = 8;
+
+	cout<<"Nilai variabel a : "<<a<<endl;
+	cout<<"Nilai variabel b : "<<b<<endl;
+	cout<<"Nilai variabel c : "<<c<<endl;
+	cout<<"Alamat dari p1 : "<<p1<<endl;
+	cout<<"Alamat dari p2 : "<<p2<<endl;
+	cout<<"Alamat dari p3 : "<<p3<<endl;
+	cout<<"Nilai yang tersimpan pada alamat p1 : "<<*p1<<endl;
+	cout<<"Nilai yang tersimpan pada alamat p2 : "<<*p2<<endl;
+	cout<<"Nilai yang tersimpan pada alamat p3 : "<<*p3<<endl;
+	
+	return 0;
+}
